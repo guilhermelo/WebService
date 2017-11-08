0079f74aa33bebb00ca97cb400331ed5e5a9f132
Guilherme <gui_meloo@hotmail.com>
2017-11-08 20:11:44 -0200

    Add final

--- src/webservice/dao/Conexao.java
+++ src/webservice/dao/Conexao.java
@@ -14,8 +14,9 @@ public class Conexao {
 		if(instance == null){
 			try {
 				DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-				//instance = DriverManager.getConnection("jdbc:mysql://192.168.0.101:3306/noticiario", "root", "mandioca");
-				instance = DriverManager.getConnection("jdbc:mariadb://192.168.0.101:3306/noticiario?user=root&password=mandioca");
+				//DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
+				instance = DriverManager.getConnection("jdbc:mysql://192.168.0.104:3306/noticiario", "root", "mandioca");
+				//instance = DriverManager.getConnection("jdbc:mariadb://192.168.0.120:3306/noticiario?user=root&password=root");
 				//192.168.0.101 root ""
 			} catch (SQLException e) {
 				System.out.println("Erro ao tentar conectar ao banco de dados: " + e.getMessage());
